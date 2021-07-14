2a407d89ebde9d43e5c6f435df88546d0e81f692
Dhruba Datta <dhrubadattaanjan@gmail.com>
2021-07-14 22:39:35 +0530

    38 solved

--- 0000 to 1299/38.cpp
+++ 0000 to 1299/38.cpp
@@ -5,6 +5,23 @@ typedef long long int ll;
 
 int main()
 {
-    
+    ll n, x;
+    cin >> n;
+    vector<pair<ll, ll>> a;
+    for (ll i = 1; i <= n; i++)
+    {
+        cin >> x;
+        a.push_back(make_pair(x, i));
+    }
+    sort(a.begin(), a.end());
+    if (a[0].first == a[1].first)
+    {
+        cout << "Still Rozdil";
+    }
+    else
+    {
+        cout << a[0].second << endl;
+    }
+
     return 0;
 }
