9a048f79e81bcc536e4608cf2680a72b5491a84c
Dhruba Datta <dhrubadattaanjan@gmail.com>
2021-07-14 23:01:44 +0530

    39 solved

---
+++ 0000 to 1299/39.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long int ll;
+
+int main()
+{
+    int a;
+    cin >> a;
+    cout << "0"
+         << " "
+         << "0"
+         << " "
+         << a;
+    return 0;
+}
