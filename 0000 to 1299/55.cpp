7efa5a5859496e594e491d06d293170c3b27615d
Dhruba Datta <dhrubadattaanjan@gmail.com>
2021-07-20 20:57:33 +0530

    6 solved

---
+++ 0000 to 1299/55.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long int ll;
+
+int main()
+{
+    int n;
+    cin >> n;
+    for (int i = 0; i <= n * 2; i++)
+    {
+        if (i!=n)
+        {
+            for (int j = 0; j < 2; j++)
+            {
+                
+            }
+            
+        }
+        
+    }
+
+    return 0;
+}
